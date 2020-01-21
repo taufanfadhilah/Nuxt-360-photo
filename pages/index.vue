80baf3e552176449d03b340549ae817cf831e9a8
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-01-21 11:12:19 +0700

    layout

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container p-5">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <b-img
+          :src="selectedImage"
+          fluid
+          rounded
+          alt="Responsive image"
+        />
+      </div>
+    </div>
+    <div class="row text-center mt-3">
+      <div class="col-md-4 my-2" v-for="image in images" :key="image">
+        <b-img
+          thumbnail
+          fluid
+          :src="image"
+          alt="Image thumbnail"
+          @click="selectImage(image)"
+        ></b-img>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      images: [
+        'img/pano 1.jpeg',
+        'img/pano 2.jpeg',
+        'img/pano 3.jpeg',
+      ],
+      selectedImage: 'img/pano 1.jpeg'
+    }
+  },
+  methods: {
+    selectImage(image){
+      this.selectedImage = image
+    }
+  }
+}
+</script>
+
+<style>
+.img-fluid {
+  max-height: 400px;
+}
+body {
+  background: #e1f5fe;
+}
+</style>
